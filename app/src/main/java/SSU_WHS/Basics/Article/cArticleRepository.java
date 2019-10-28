1d2afd5fdcbf8777c8f66061c90170ce5b9a1215
Nigel Kraaij <n.kraaij@ams.icsvertex.nl>
2019-10-28 10:10:39 +0100

    -Missing files

---
+++ app/src/main/java/SSU_WHS/Basics/Article/cArticleRepository.java
@@ -0,0 +1,231 @@
+package SSU_WHS.Basics.Article;
+
+import android.os.AsyncTask;
+import org.json.JSONException;
+import org.ksoap2.serialization.PropertyInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import ICS.Utils.Scanning.cBarcodeScan;
+import SSU_WHS.Basics.Users.cUser;
+import SSU_WHS.Webservice.cWebresult;
+import SSU_WHS.Webservice.cWebserviceDefinitions;
+
+public class cArticleRepository {
+
+    //Region Public Properties
+
+    //End Region Public Properties
+
+    //Region Private Properties
+
+
+    //End Region Private Properties
+
+    //Region Constructor
+    cArticleRepository() {
+
+    }
+    //End Region Constructor
+
+    //Region Public Methods
+
+    public cWebresult pGetArticleViaBarcodeWrs(cBarcodeScan pvBarcodeScan) {
+
+        List<String> resultObl = new ArrayList<>();
+        cWebresult webResultWrs = new cWebresult();
+
+        try {
+            webResultWrs = new mGetArticleViaScannedBarcodeFromWebserviceGetAsyncTask().execute(pvBarcodeScan).get();
+        } catch (ExecutionException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        }
+        return webResultWrs;
+    }
+
+    public cWebresult pGetBarcodesWrs(cArticle pvArticle) {
+
+        List<String> resultObl = new ArrayList<>();
+        cWebresult webResultWrs = new cWebresult();
+
+        try {
+            webResultWrs = new mGetBarcodesFromWebserviceGetAsyncTask().execute(pvArticle).get();
+        } catch (ExecutionException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        }
+        return webResultWrs;
+    }
+
+    public cWebresult pGetStockWrs(cArticle pvArticle) {
+
+        List<String> resultObl = new ArrayList<>();
+        cWebresult webResultWrs = new cWebresult();
+
+        try {
+            webResultWrs = new mGetStockFromWebserviceGetAsyncTask().execute(pvArticle).get();
+        } catch (ExecutionException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            webResultWrs.setResultBln(false);
+            webResultWrs.setSuccessBln(false);
+            resultObl.add(e.getLocalizedMessage());
+            webResultWrs.setResultObl(resultObl);
+            e.printStackTrace();
+        }
+        return webResultWrs;
+    }
+
+    //End Region Public Methods
+
+    //Region Private Methods
+
+    //End Region Private Methods
+
+    private static class mGetArticleViaScannedBarcodeFromWebserviceGetAsyncTask extends AsyncTask<cBarcodeScan, Void, cWebresult> {
+        @Override
+        protected cWebresult doInBackground(final cBarcodeScan... params) {
+            cWebresult WebresultWrs = new cWebresult();
+
+            List<PropertyInfo> l_PropertyInfoObl = new ArrayList<>();
+
+            PropertyInfo l_PropertyInfo1Pin = new PropertyInfo();
+            l_PropertyInfo1Pin.name = cWebserviceDefinitions.WEBPROPERTY_USERNAMEDUTCH;
+            l_PropertyInfo1Pin.setValue(cUser.currentUser.getNameStr());
+            l_PropertyInfoObl.add(l_PropertyInfo1Pin);
+
+            PropertyInfo l_PropertyInfo2Pin = new PropertyInfo();
+            l_PropertyInfo2Pin.name = cWebserviceDefinitions.WEBPROPERTY_OWNER;
+            l_PropertyInfo2Pin.setValue("");
+            l_PropertyInfoObl.add(l_PropertyInfo2Pin);
+
+            PropertyInfo l_PropertyInfo3Pin = new PropertyInfo();
+            l_PropertyInfo3Pin.name = cWebserviceDefinitions.WEBPROPERTY_BARCODE;
+            l_PropertyInfo3Pin.setValue(params[0].getBarcodeStr());
+            l_PropertyInfoObl.add(l_PropertyInfo3Pin);
+
+            PropertyInfo l_PropertyInfo4Pin = new PropertyInfo();
+            l_PropertyInfo4Pin.name = cWebserviceDefinitions.WEBPROPERTY_BARCODEORIGINAL;
+            l_PropertyInfo4Pin.setValue(params[0].getBarcodeOriginalStr());
+            l_PropertyInfoObl.add(l_PropertyInfo4Pin);
+
+            try {
+                WebresultWrs = new cWebresult().pGetwebresultWrs(cWebserviceDefinitions.WEBMETHOD_GETARTICLEVIAOWNERBARCODE, l_PropertyInfoObl);
+            } catch (JSONException e) {
+                WebresultWrs.setResultBln(false);
+                WebresultWrs.setSuccessBln(false);
+                e.printStackTrace();
+            }
+
+            return WebresultWrs;
+        }
+    }
+
+    private static class mGetBarcodesFromWebserviceGetAsyncTask extends AsyncTask<cArticle, Void, cWebresult> {
+        @Override
+        protected cWebresult doInBackground(final cArticle... params) {
+            cWebresult WebresultWrs = new cWebresult();
+
+            List<PropertyInfo> l_PropertyInfoObl = new ArrayList<>();
+
+            PropertyInfo l_PropertyInfo1Pin = new PropertyInfo();
+            l_PropertyInfo1Pin.name = cWebserviceDefinitions.WEBPROPERTY_USERNAMEDUTCH;
+            l_PropertyInfo1Pin.setValue(cUser.currentUser.getNameStr());
+            l_PropertyInfoObl.add(l_PropertyInfo1Pin);
+
+            PropertyInfo l_PropertyInfo2Pin = new PropertyInfo();
+            l_PropertyInfo2Pin.name = cWebserviceDefinitions.WEBPROPERTY_OWNER;
+            l_PropertyInfo2Pin.setValue("");
+            l_PropertyInfoObl.add(l_PropertyInfo2Pin);
+
+            PropertyInfo l_PropertyInfo3Pin = new PropertyInfo();
+            l_PropertyInfo3Pin.name = cWebserviceDefinitions.WEBPROPERTY_ITEMNO;
+            l_PropertyInfo3Pin.setValue(params[0].getItemNoStr());
+            l_PropertyInfoObl.add(l_PropertyInfo3Pin);
+
+            PropertyInfo l_PropertyInfo4Pin = new PropertyInfo();
+            l_PropertyInfo4Pin.name = cWebserviceDefinitions.WEBPROPERTY_VARIANTCODE;
+            l_PropertyInfo4Pin.setValue(params[0].getVariantCodeStr());
+            l_PropertyInfoObl.add(l_PropertyInfo4Pin);
+
+            try {
+                WebresultWrs = new cWebresult().pGetwebresultWrs(cWebserviceDefinitions.WEBMETHOD_GETARTICLEBARCODES, l_PropertyInfoObl);
+            } catch (JSONException e) {
+                WebresultWrs.setResultBln(false);
+                WebresultWrs.setSuccessBln(false);
+                e.printStackTrace();
+            }
+
+            return WebresultWrs;
+        }
+    }
+
+    private static class mGetStockFromWebserviceGetAsyncTask extends AsyncTask<cArticle, Void, cWebresult> {
+        @Override
+        protected cWebresult doInBackground(final cArticle... params) {
+            cWebresult WebresultWrs = new cWebresult();
+
+            List<PropertyInfo> l_PropertyInfoObl = new ArrayList<>();
+
+            PropertyInfo l_PropertyInfo1Pin = new PropertyInfo();
+            l_PropertyInfo1Pin.name = cWebserviceDefinitions.WEBPROPERTY_USERNAMEDUTCH;
+            l_PropertyInfo1Pin.setValue(cUser.currentUser.getNameStr());
+            l_PropertyInfoObl.add(l_PropertyInfo1Pin);
+
+            PropertyInfo l_PropertyInfo2Pin = new PropertyInfo();
+            l_PropertyInfo2Pin.name = cWebserviceDefinitions.WEBPROPERTY_LOCATION_NL;
+            l_PropertyInfo2Pin.setValue(cUser.currentUser.currentBranch.getBranchStr());
+            l_PropertyInfoObl.add(l_PropertyInfo2Pin);
+
+            PropertyInfo l_PropertyInfo3Pin = new PropertyInfo();
+            l_PropertyInfo3Pin.name = cWebserviceDefinitions.WEBPROPERTY_OWNER;
+            l_PropertyInfo3Pin.setValue("");
+            l_PropertyInfoObl.add(l_PropertyInfo3Pin);
+
+            PropertyInfo l_PropertyInfo4Pin = new PropertyInfo();
+            l_PropertyInfo4Pin.name = cWebserviceDefinitions.WEBPROPERTY_ITEMNO;
+            l_PropertyInfo4Pin.setValue(params[0].getItemNoStr());
+            l_PropertyInfoObl.add(l_PropertyInfo4Pin);
+
+            PropertyInfo l_PropertyInfo5Pin = new PropertyInfo();
+            l_PropertyInfo5Pin.name = cWebserviceDefinitions.WEBPROPERTY_VARIANTCODE;
+            l_PropertyInfo5Pin.setValue(params[0].getVariantCodeStr());
+            l_PropertyInfoObl.add(l_PropertyInfo5Pin);
+
+            try {
+                WebresultWrs = new cWebresult().pGetwebresultWrs(cWebserviceDefinitions.WEBMETHOD_GETARTICLESTOCK, l_PropertyInfoObl);
+            } catch (JSONException e) {
+                WebresultWrs.setResultBln(false);
+                WebresultWrs.setSuccessBln(false);
+                e.printStackTrace();
+            }
+
+            return WebresultWrs;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/SSU_WHS/Basics/Article/cArticleViewModel.java
@@ -0,0 +1,25 @@
+package SSU_WHS.Basics.Article;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+
+import ICS.Utils.Scanning.cBarcodeScan;
+import SSU_WHS.Webservice.cWebresult;
+
+public class cArticleViewModel extends AndroidViewModel {
+
+
+    private cArticleRepository Repository;
+
+    public cArticleViewModel(Application application) {
+        super(application);
+        Repository = new cArticleRepository();
+    }
+
+    public cWebresult pGetArticleByBarcodeViaWebserviceWrs(cBarcodeScan pvBarcodeScan) {return this.Repository.pGetArticleViaBarcodeWrs(pvBarcodeScan);}
+    public cWebresult pGetBarcodesViaWebserviceWrs(cArticle pvArticle) {return this.Repository.pGetBarcodesWrs(pvArticle);}
+    public cWebresult pGetStockViaWebserviceWrs(cArticle pvArticle) {return this.Repository.pGetStockWrs(pvArticle);}
+
+
+}
